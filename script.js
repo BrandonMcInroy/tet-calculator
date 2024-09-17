b42336ff7debf4056698924c2202d2336f722c0a
Brandon McInroy <53107062+BrandonMcInroy@users.noreply.github.com>
2024-09-17 11:24:02 -0700

    Add getelements to JS and testing

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log("Hello World");
+let startOne = document.getElementById("startOne").value;
+let endOne = document.getElementById("endOne").value;
+let startTwo = document.getElementById("startTwo").value;
+let endTwo = document.getElementById("endTwo").value;
+
+console.log(startOne);
+console.log(endOne);
+console.log(startTwo);
+console.log(endTwo);
