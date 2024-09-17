f718efac4b00044c2d643f1df79419bd0474c42f
Brandon McInroy <53107062+BrandonMcInroy@users.noreply.github.com>
2024-09-17 11:33:17 -0700

    Created action for form submit. consolelog details

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-console.log("Hello World");
-let startOne = document.getElementById("startOne").value;
-let endOne = document.getElementById("endOne").value;
-let startTwo = document.getElementById("startTwo").value;
-let endTwo = document.getElementById("endTwo").value;
+let timeForm = document.getElementById("timeForm");
 
-console.log(startOne);
-console.log(endOne);
-console.log(startTwo);
-console.log(endTwo);
+timeForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  // Get elements
+  let startOne = document.getElementById("startOne").value;
+  let endOne = document.getElementById("endOne").value;
+  let startTwo = document.getElementById("startTwo").value;
+  let endTwo = document.getElementById("endTwo").value;
+  console.log(startOne);
+  console.log(endOne);
+  console.log(startTwo);
+  console.log(endTwo);
+  console.log("Submitted");
+});
