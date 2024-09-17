106885595e83415a5d994837ddd857bf12e9e650
Brandon McInroy <53107062+BrandonMcInroy@users.noreply.github.com>
2024-09-17 11:45:20 -0700

    Playing around with the from details

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let timeForm = document.getElementById("timeForm");
+const timeForm = document.getElementById("timeForm");
 
 timeForm.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -7,6 +7,33 @@ timeForm.addEventListener("submit", (e) => {
   let endOne = document.getElementById("endOne").value;
   let startTwo = document.getElementById("startTwo").value;
   let endTwo = document.getElementById("endTwo").value;
+
+  const startOneTime = startOne.split(":");
+  const endOneTime = endOne.split(":");
+  const startTwoTime = startTwo.split(":");
+  const endTwoTime = endTwo.split(":");
+
+  // Convert to milliseconds
+  startOneTime[0] = startOneTime[0] * 60 * 1000;
+  startOneTime[1] = startOneTime[1] * 1000;
+  endOneTime[0] = endOneTime[0] * 60 * 1000;
+  endOneTime[1] = endOneTime[1] * 1000;
+  startTwoTime[0] = startTwoTime[0] * 60 * 1000;
+  startTwoTime[1] = startTwoTime[1] * 1000;
+  endTwoTime[0] = endTwoTime[0] * 60 * 1000;
+  endTwoTime[1] = endTwoTime[1] * 1000;
+
+  const totalTime = endOneTime - startOneTime + endTwoTime - startTwoTime;
+
+  document.getElementById(
+    "shiftOne"
+  ).innerHTML = `Shift One: ${startOneTime.toLocaleTimeString()} - ${endOneTime.toLocaleTimeString()}`;
+  document.getElementById(
+    "shiftTwo"
+  ).innerHTML = `Shift Two: ${startTwoTime.toLocaleTimeString()} - ${endTwoTime.toLocaleTimeString()}`;
+  document.getElementById(
+    "totalTime"
+  ).innerHTML = `Total Time: ${totalTime.toLocaleTimeString()}`;
   console.log(startOne);
   console.log(endOne);
   console.log(startTwo);
