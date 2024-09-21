456c7706374f20c0ffe2f206f60f58b16aa04cd1
Brandon McInroy <53107062+BrandonMcInroy@users.noreply.github.com>
2024-09-21 12:27:08 -0700

    Removed the earliest/latest start/finish logic fn

--- script.js
+++ script.js
@@ -65,48 +65,6 @@ timeForm.addEventListener("submit", (e) => {
   ).textContent = `Hours available for OT: ${formatDecimalHoursToHHMM(
     remainingDriveHours
   )}`;
-
-  // Calculate the earliest possible start time based on maxDutyHours and end times
-  const earliestPossibleStartTime = new Date(endTimeTwo);
-  earliestPossibleStartTime.setHours(
-    earliestPossibleStartTime.getHours() - maxDutyHours
-  );
-
-  // Ensure the shift starts within the allowed time frame
-  if (startTimeOne < earliestPossibleStartTime) {
-    // Handle the case where the shift starts too early (e.g., display an error message)
-    document.getElementById(
-      "earliestStart"
-    ).textContent = `Shift starts too early`;
-  } else {
-    document.getElementById(
-      "earliestStart"
-    ).textContent = `Earliest start time: ${formatTimeWithoutSeconds(
-      earliestPossibleStartTime
-    )}`;
-  }
-  // Calculate the latest possible end time based on maxDutyHours
-  const latestPossibleEndTime = new Date(startTimeOne);
-  latestPossibleEndTime.setHours(
-    latestPossibleEndTime.getHours() + maxDutyHours
-  );
-
-  // Ensure the shift ends within maxDutyHours
-  if (
-    endTimeOne > latestPossibleEndTime ||
-    (endTimeTwo && endTimeTwo > latestPossibleEndTime)
-  ) {
-    // Handle the case where the shift exceeds maxDutyHours (e.g., display an error message)
-    document.getElementById(
-      "latestFinish"
-    ).textContent = `Shift exceeds maximum allowed time`;
-  } else {
-    document.getElementById(
-      "latestFinish"
-    ).textContent = `Latest finish time: ${formatTimeWithoutSeconds(
-      latestPossibleEndTime
-    )}`;
-  }
 });
 
 function normalize(inputTime) {
